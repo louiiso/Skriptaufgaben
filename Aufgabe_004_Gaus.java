00025bbb2acf1c72b2c95be873b5ab48b62f4f0a
louiiso <153605246+louiiso@users.noreply.github.com>
2026-03-01 13:17:30 +0100

    Test ppush
    testpush

--- Aufgabe_004_Gaus.java
+++ Aufgabe_004_Gaus.java
@@ -3,7 +3,7 @@ public class Aufgabe_004_Gaus
 	{
 		public static void main (String args[]) 
 		{
-			int summe = 0;
+			int summe = 0;  //testkommetar 
 			for (int i=1; i <= 100; i++)
 			summe = summe + i;
 			System.out.println(summe);
