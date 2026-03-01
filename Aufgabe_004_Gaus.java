a5331612f558f8c9189c3ba98491ec4695a88733
louiiso <153605246+louiiso@users.noreply.github.com>
2026-03-01 13:27:52 +0100

    second commit
    correction of the comment

--- Aufgabe_004_Gaus.java
+++ Aufgabe_004_Gaus.java
@@ -3,7 +3,7 @@ public class Aufgabe_004_Gaus
 	{
 		public static void main (String args[]) 
 		{
-			int summe = 0;  //testkommetar 
+			int summe = 0;  //testkommentar 
 			for (int i=1; i <= 100; i++)
 			summe = summe + i;
 			System.out.println(summe);
